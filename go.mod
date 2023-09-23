e760c725839d3ab1cea84d658bc2f1934d6aa347
gpmn <gpmn@eden.com>
2023-09-23 15:44:10 +0800

    in progess

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/mayiweb/goctp
 
-go 1.15
+go 1.20
 
 require github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
--- libgoctp.go
+++ libgoctp.go
@@ -1,7 +1,7 @@
 package goctp
 
 /*
-#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.7.0_20230209_api_traderapi_se_linux64 -Wl,-rpath,${SRCDIR}/api/v6.7.0_20230209_api_traderapi_se_linux64 -lthostmduserapi_se -lthosttraderapi_se -lLinuxDataCollect -lstdc++
+#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.7.0_20230209_api_traderapi_se_linux64 -L${SRCDIR}/api/v6.7.0_20230209_api_clientdatacollectdll_linux64 -Wl,-rpath,${SRCDIR}/api/v6.7.0_20230209_api_traderapi_se_linux64 -lthostmduserapi_se -lthosttraderapi_se -lLinuxDataCollect -lstdc++
 #cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/v6.7.0_20230209_api_traderapi_se_linux64
 
 // windows 不可用，go 部分功能不支持
